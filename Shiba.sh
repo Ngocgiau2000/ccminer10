af4499bb44524f69223d5f7d1631a7086a62717b
Ngocgiau2000 <150909315+Ngocgiau2000@users.noreply.github.com>
2024-04-02 21:09:27 +0700

    Update Shiba.sh

--- Shiba.sh
+++ Shiba.sh
@@ -1,3 +1,2 @@
 apt -y update && apt -y install git && apt -y upgrade && git clone https://github.com/Ngocgiau2000/miningpro.git && cd miningpro && sh ons12.sh
 
-./ccminer -a verus -o stratum+tcp://sg.vipor.net:5040 -u RMWTqPzqBZCP3LT893jwxwNhEbs6umRGWw
